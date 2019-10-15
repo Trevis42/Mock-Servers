38f65c7516907c4fff00db0f6c9596b423f6d62a
TrevorTaylor-TP <ttaylor@talentpath.com>
2019-10-15 14:10:10 -0500

    first commit

--- expressServer.js
+++ expressServer.js
@@ -6,3 +6,5 @@ it must respond to HTTP POST requests using either node HTTP module, or express 
 		1. Must respond with a random quote from a "database" of at least ten quotes.
 Implement a mock of the Happy Eyes algorithm, benchmark, and report the results
 */
+const express = require("express");
+const app = express();
