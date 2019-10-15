7dff8e57822a0579e50ac55e6317c7fb4ce2872d
TrevorTaylor-TP <ttaylor@talentpath.com>
2019-10-15 14:02:06 -0500

    initial setup, comment about the project

---
+++ expressServer.js
@@ -0,0 +1,8 @@
+/*
+Use express to create a server:
+
+it must respond to HTTP POST requests using either node HTTP module, or express module
+	Response:
+		1. Must respond with a random quote from a "database" of at least ten quotes.
+Implement a mock of the Happy Eyes algorithm, benchmark, and report the results
+*/
